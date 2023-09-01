3fed773a306ca24ad30b2ad08778e4f416142a10
unknown <karishmatejwani9@gmail.com>
2023-09-01 23:07:51 +0500

    Post

---
+++ pages/components/OtherPost.jsx
@@ -0,0 +1,62 @@
+import {
+  Box,
+  Card,
+  CardHeader,
+  Avatar,
+  IconButton,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActions,
+  Checkbox,
+} from '@mui/material';
+import React from 'react';
+import { purple } from '@mui/material/colors';
+import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+const OtherPost = () => {
+  return (
+    <Box>
+      <Card>
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: purple[300] }} aria-label="recipe">
+              N
+            </Avatar>
+          }
+          action={
+            <IconButton aria-label="settings">
+              <MoreVertIcon />
+            </IconButton>
+          }
+          title="Nick Jonas"
+          subheader="September 05, 2023"
+        />
+        <CardMedia component="img" image="/assets/pic.jpg" alt="Scenery" />
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            This impressive paella is a perfect party dish and a fun meal to
+            cook together with your guests. Add 1 cup of frozen peas along with
+            the mussels, if you like.
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+            <Checkbox
+              icon={<FavoriteBorder />}
+              checkedIcon={<FavoriteIcon sx={{ color: 'red' }} />}
+            />
+          </IconButton>
+          <IconButton aria-label="share">
+            <ShareIcon />
+          </IconButton>
+        </CardActions>
+      </Card>
+    </Box>
+  );
+};
+
+export default OtherPost;
