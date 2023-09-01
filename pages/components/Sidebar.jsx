11c8fcd31750a38f7ffadaf82e57ee129ee7a715
unknown <karishmatejwani9@gmail.com>
2023-09-01 21:19:33 +0500

    Sidebar

--- pages/components/Sidebar.jsx
+++ pages/components/Sidebar.jsx
@@ -1,10 +1,91 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import {
+  Box,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Switch,
+} from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import GroupIcon from '@mui/icons-material/Group';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import GroupsIcon from '@mui/icons-material/Groups';
+import StoreIcon from '@mui/icons-material/Store';
+import WorkHistoryIcon from '@mui/icons-material/WorkHistory';
+import ArticleIcon from '@mui/icons-material/Article';
+import ModeNightIcon from '@mui/icons-material/ModeNight';
 
 const Sidebar = () => {
   return (
-    <Box bgcolor="pink" flex={1} p={2} sx={{ display : {xs:'none', sm:'block'}}}>
-      Sidebar
+    <Box flex={1} p={2} sx={{ display: { xs: 'none', sm: 'block' } }}>
+      <List
+        sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+        component="nav"
+      >
+        <ListItemButton>
+          <ListItemIcon>
+            <HomeIcon />
+          </ListItemIcon>
+          <ListItemText primary="Homepage" />
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon>
+            <BookmarkIcon />
+          </ListItemIcon>
+          <ListItemText primary="Saved" />
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon>
+            <GroupsIcon />
+          </ListItemIcon>
+          <ListItemText primary="Groups" />
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon>
+            <StoreIcon />
+          </ListItemIcon>
+          <ListItemText primary="Marketplace" />
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon>
+            <GroupIcon />
+          </ListItemIcon>
+          <ListItemText primary="Friends" />
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon>
+            <WorkHistoryIcon />
+          </ListItemIcon>
+          <ListItemText primary="Feeds" />
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon>
+            <ArticleIcon />
+          </ListItemIcon>
+          <ListItemText primary="Pages" />
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon>
+            <AccountBoxIcon />
+          </ListItemIcon>
+          <ListItemText primary="Profile" />
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon>
+            <SettingsIcon />
+          </ListItemIcon>
+          <ListItemText primary="Settings" />
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon>
+            <ModeNightIcon />
+          </ListItemIcon>
+          <Switch />
+        </ListItemButton>
+      </List>
     </Box>
   );
 };
