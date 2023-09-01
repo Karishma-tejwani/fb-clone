cb843b9a798116a3c3e00eb26e6afb099c3282af
unknown <karishmatejwani9@gmail.com>
2023-09-01 23:24:54 +0500

    Sidebar

--- pages/components/Sidebar.jsx
+++ pages/components/Sidebar.jsx
@@ -22,71 +22,71 @@ const Sidebar = () => {
   return (
     <Box flex={1} p={2} sx={{ display: { xs: 'none', sm: 'block' } }}>
       <Box position="fixed">
-        <List
-          sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
-          component="nav"
-        >
-          <ListItemButton>
-            <ListItemIcon>
-              <HomeIcon />
-            </ListItemIcon>
-            <ListItemText primary="Homepage" />
-          </ListItemButton>
-          <ListItemButton>
-            <ListItemIcon>
-              <BookmarkIcon />
-            </ListItemIcon>
-            <ListItemText primary="Saved" />
-          </ListItemButton>
-          <ListItemButton>
-            <ListItemIcon>
-              <GroupsIcon />
-            </ListItemIcon>
-            <ListItemText primary="Groups" />
-          </ListItemButton>
-          <ListItemButton>
-            <ListItemIcon>
-              <StoreIcon />
-            </ListItemIcon>
-            <ListItemText primary="Marketplace" />
-          </ListItemButton>
-          <ListItemButton>
-            <ListItemIcon>
-              <GroupIcon />
-            </ListItemIcon>
-            <ListItemText primary="Friends" />
-          </ListItemButton>
-          <ListItemButton>
-            <ListItemIcon>
-              <WorkHistoryIcon />
-            </ListItemIcon>
-            <ListItemText primary="Feeds" />
-          </ListItemButton>
-          <ListItemButton>
-            <ListItemIcon>
-              <ArticleIcon />
-            </ListItemIcon>
-            <ListItemText primary="Pages" />
-          </ListItemButton>
-          <ListItemButton>
-            <ListItemIcon>
-              <AccountBoxIcon />
-            </ListItemIcon>
-            <ListItemText primary="Profile" />
-          </ListItemButton>
-          <ListItemButton>
-            <ListItemIcon>
-              <SettingsIcon />
-            </ListItemIcon>
-            <ListItemText primary="Settings" />
-          </ListItemButton>
-          <ListItemButton>
-            <ListItemIcon>
-              <ModeNightIcon />
-            </ListItemIcon>
-            <Switch />
-          </ListItemButton>
-        </List>
+      <List
+        sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+        component="nav"
+      >
+        <ListItemButton>
+          <ListItemIcon>
+            <HomeIcon />
+          </ListItemIcon>
+          <ListItemText primary="Homepage" />
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon>
+            <BookmarkIcon />
+          </ListItemIcon>
+          <ListItemText primary="Saved" />
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon>
+            <GroupsIcon />
+          </ListItemIcon>
+          <ListItemText primary="Groups" />
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon>
+            <StoreIcon />
+          </ListItemIcon>
+          <ListItemText primary="Marketplace" />
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon>
+            <GroupIcon />
+          </ListItemIcon>
+          <ListItemText primary="Friends" />
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon>
+            <WorkHistoryIcon />
+          </ListItemIcon>
+          <ListItemText primary="Feeds" />
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon>
+            <ArticleIcon />
+          </ListItemIcon>
+          <ListItemText primary="Pages" />
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon>
+            <AccountBoxIcon />
+          </ListItemIcon>
+          <ListItemText primary="Profile" />
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon>
+            <SettingsIcon />
+          </ListItemIcon>
+          <ListItemText primary="Settings" />
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon>
+            <ModeNightIcon />
+          </ListItemIcon>
+          <Switch />
+        </ListItemButton>
+      </List>
       </Box>
     </Box>
   );
